929b39e057bd6109698e20ea92b756cc30c66126
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-08-16 09:06:47 +0100

    Add VuePress v2

---
+++ .vuepress/config.js
@@ -0,0 +1,50 @@
+const { description } = require('../package')
+
+module.exports = {
+  title: 'Sleep Diary Docs',
+  description: description,
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  themeConfig: {
+
+    repo: 'https://github.com/sleepdiary/docs',
+
+    lastUpdated: true,
+
+    contributors: false,
+
+    sidebar: [
+        {
+            text: 'Create a diary',
+            link: '/create/',
+            children: [
+                '/create/formats',
+                '/create/reconstructing',
+            ]
+        },
+        {
+            text: 'Sleeping patterns',
+            link: '/patterns/',
+            children: [
+                '/patterns/day-length',
+                '/patterns/late-sleep',
+                '/patterns/sunlight',
+            ]
+        },
+        {
+            text: 'Development',
+            link: '/development/',
+            children: [
+                '/development/maintainer-environment-recommendations',
+                '/development/minimising-planned-maintenance',
+                '/development/pull-request-review-checklist',
+            ]
+        },
+    ],
+  },
+}
