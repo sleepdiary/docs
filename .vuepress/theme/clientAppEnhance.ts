7c9204c22d8e437d222c45bb3e72f9b44fd4b227
Andrew Sayers <andrew-github.com@pileofstuff.org>
2022-03-20 18:20:05 +0000

    Add SortableTable

--- .vuepress/theme/clientAppEnhance.ts
+++ .vuepress/theme/clientAppEnhance.ts
@@ -3,10 +3,12 @@ import ImageFrame from './page-components/ImageFrame.vue'
 import ImageGallery from './page-components/ImageGallery.vue'
 import Simulate from './page-components/Simulate.vue'
 import Editor from './page-components/Editor.vue'
+import SortableTable from './page-components/SortableTable.vue'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.component('ImageFrame', ImageFrame);
   app.component('ImageGallery', ImageGallery);
   app.component('Simulate', Simulate);
   app.component('Editor', Editor);
+  app.component('SortableTable', SortableTable);
 })
