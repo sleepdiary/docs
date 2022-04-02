ccafc7ca172504a3407811fbecf1bca23249ecad
Andrew Sayers <andrew-github.com@pileofstuff.org>
2022-04-02 21:06:16 +0100

    Add ImageGallery

--- .vuepress/theme/clientAppEnhance.ts
+++ .vuepress/theme/clientAppEnhance.ts
@@ -1,10 +1,12 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import ImageFrame from './page-components/ImageFrame.vue'
+import ImageGallery from './page-components/ImageGallery.vue'
 import Simulate from './page-components/Simulate.vue'
 import Editor from './page-components/Editor.vue'
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.component('ImageFrame', ImageFrame);
+  app.component('ImageGallery', ImageGallery);
   app.component('Simulate', Simulate);
   app.component('Editor', Editor);
 })
